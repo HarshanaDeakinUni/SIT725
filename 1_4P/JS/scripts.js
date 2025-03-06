0dead31d480da228851135ce8a87d6f397a4ecb9
HarshanaDeakinUni <s224714499@deakin.edu.au>
2025-03-06 16:39:24 +1100

    code improvements

--- 1_4P/JS/scripts.js
+++ 1_4P/JS/scripts.js
@@ -1,11 +1,23 @@
 function changeText(){
-        var textsArray = ["Text 1","Text 2", "Text 3", "Text 4", "Text 5"]
-        var number = getRandomNumberBetween(0,textsArray.length - 1)
-        console.log("Index: ", number)
-        document.getElementById("heading").innerHTML = textsArray[number];
+        var textsArray = ["Text 1","Text 2", "Text 3", "Text 4", "Text 5"]
+        var number = getRandomNumberBetween(0,textsArray.length - 1)
+        console.log("Index: ", number)
+        var element = document.getElementById("heading");
+        element.innerHTML = textsArray[number];
+        element.style.color = getRandomColour();
+
     }
     
     function getRandomNumberBetween(min,max){
-        return Math.floor(Math.random()*(max-min+1)+min);
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
+
+    function getRandomColour(){
+        var letters = '0123456789ABCDEF';
+        var colour = '#';
+        for (var i = 0; i < 6; i++) {
+            colour += letters[Math.floor(Math.random() * 16)];
+        }
+        return colour;
     }
     
